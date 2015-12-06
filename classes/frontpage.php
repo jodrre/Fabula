2934545082e0fdd328c8ee57d451e1f2f669962b
jodrre <jodrre@gmail.com>
2015-12-06 20:09:23 +0000

    I don't even know what anymore.

---
+++ classes/frontpage.php
@@ -0,0 +1,6 @@
+<?php
+
+$siteform_login = '<form name="login" method="POST" enctype="multipart/form-data">'.
+'Username: <input type="text" name="username" value="" /><br />'.
+'Password: <input type="password" name="pass" value="" /><br />'.
+'<input type="submit" value="Login" name="btnLogin" /></form>';
--- inc/conf.php
+++ inc/conf.php
@@ -1,16 +1,34 @@
 <?php
 
-$db_host = '';
-$db_db = '';
-$db_user = '';
-$db_pass = '';
+define( "DB_DSN", "mysql:host=localhost;dbname=fabula" );
+define( "DB_USERNAME", "jodrre" );
+define( "DB_PASSWORD", "" );
 
+define( "ADMIN_USERNAME", "admin" );
+define( "ADMIN_PASSWORD", "mypass" );
+
+date_default_timezone_set( "America/Los_Angeles" );  // http://www.php.net/manual/en/timezones.php
+
+// These are for testing and should be commented out before putting in a prod environment
+$user_login_status = 0;
 
 /* 
  * Please do not make changes below this line
  * ==========================================
  */
-$dbconn = mysqli_connect($db_host, $db_user, $db_pass, $db_db);
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_db);
 
-// These are for testing and should be commented out before putting in a prod environment
-$user_login_status = 0;
+ini_set( "display_errors", true );
+// Change to the correct path if not installed in root of web
+define( "APP_PATH", $_SERVER['DOCUMENT_ROOT']);
+define( "CLASS_PATH", "../classes" );
+define( "TEMPLATE_PATH", "../templates" );
+define( "HOMEPAGE_NUM_ARTICLES", 5 );
+// require( CLASS_PATH . "/Article.php" );
+
+function handleException( $exception ) {
+  echo "Sorry, a problem occurred. Please try later.";
+  error_log( $exception->getMessage() );
+}
+
+set_exception_handler( 'handleException' );
